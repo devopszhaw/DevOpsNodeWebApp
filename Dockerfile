837e578eda7f50647a071e577de731f6d9d88189
mosazhaw <adrian.moser@zhaw.ch>
2025-11-27 11:23:50 +0100

    fs26

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,7 @@
 # docker build -t mosazhaw/node-web-app .
+# docker run -p 3001:3000 --name expressapp -d mosazhaw/node-web-app
 
-FROM node:22.15.0
+FROM node:24.11.1
 
 # Create app directory
 WORKDIR /usr/src/app
--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello FS2025 DevOps Course! Now Docker Deployment with GitHub Actions!!!');
+  res.send('Hello FS2026 DevOps Course!');
 });
 
 app.listen(PORT, HOST);
