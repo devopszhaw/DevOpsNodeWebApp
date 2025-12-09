bf3eccddcfd9f4c5b56f4be932d8d17e918b63d2
mosazhaw <adrian.moser@zhaw.ch>
2025-12-09 14:22:02 +0100

    test commit

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello FS2026 DevOps Course on Azure! :-)');
+  res.send('Hello FS2026 DevOps Course on MS Azure! :-)');
 });
 
 app.listen(PORT, HOST);
